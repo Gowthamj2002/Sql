2376b6d30e99634a44eb2d7a18930704db5cda27
unknown <gowthamj.xworkz@gmail.com>
2024-06-26 23:02:27 +0530

    june_26

---
+++ project.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE project;
+USE project;
+CREATE TABLE Airport_details(id int, A_name char, A_arrival char, A_departure char, price bigint);
+CREATE TABLE cricket_team(id int, c_name char,no_of_players int, c_captain char, c_coach char);
